de69350dace8d647976e1713674c0a9cb38b3252
mbl111 <matt@mbl111.com>
2013-12-13 17:02:01 +1100

    Gotta head out quickly... Cutting it here after I have done some of the basic code

---
+++ src/main/java/net/specialattack/modjam/ModJam.java
@@ -0,0 +1,40 @@
+package net.specialattack.modjam;
+
+import net.minecraftforge.common.Configuration;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Objects.MOD_ID, version = Objects.MOD_VERSION, name=Objects.MOD_NAME)
+public class ModJam {
+
+	@Instance
+	public static ModJam instance;
+
+	public Configuration config;
+	
+	@SidedProxy(clientSide=Objects.PROXY_CLIENT, serverSide=Objects.PROXY_COMMON)
+	public static CommonProxy proxy;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+		this.config = new Configuration(event.getSuggestedConfigurationFile());
+		
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+
+	}
+
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+
+	}
+
+}
